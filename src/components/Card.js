d460a92c55d6bdd96bcab06d649c18df4aeba36b
Durvesh7k <dschopade7k@gmail.com>
2023-08-04 01:35:40 +0530

    changes:

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const Card = (props) => {
     return (
-        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" className="px-4 py-3 md:px-6 md:py-4 font-medium text-gray-900 dark:text-white">
+        <tr className="bg-white   dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <th scope="row" className="px-4 py-3 md:px-12 md:py-4 font-medium text-gray-900 dark:text-white">
                 {props.rank}
             </th>
             <td className="px-4 py-3 md:px-6 md:py-4">
-                <div className="flex gap-1 justify-left items-center">
-                    <img src={props.image} alt="coin" className="w-8" />
-                    <p>{props.name}</p>
+                <div className="flex md:flex-row  flex-col gap-1 justify-left items-center">
+                    <div className='flex flex-row items-center gap-1'>
+                        <img src={props.image} alt="coin" className="w-8" />
+                        <p>{props.name}</p>
+                    </div>
                     <p className="opacity-75">{props.symbol}</p>
                 </div>
             </td>
@@ -29,7 +31,7 @@ const Card = (props) => {
             <td className="px-4 py-3 md:px-6 md:py-4 text-center">
                 ${props.volume.toLocaleString()}
             </td>
-            <td className="px-4 py-3 md:px-6 md:py-4 text-center">
+            <td className="px-4 py-3 md:px-28 md:py-4 text-center">
                 ${props.market_cap.toLocaleString()}
             </td>
         </tr>
--- src/components/CryptoApi2.js
+++ src/components/CryptoApi2.js
@@ -12,7 +12,7 @@ const CryptoApi = (props) => {
   useEffect(() => {
     async function getData() {
       try {
-        const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
+        const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false`)
         setData(res.data);
       }
       catch (err) {
@@ -26,26 +26,26 @@ const CryptoApi = (props) => {
   return (
 
 
-    <div class=" p-12 shadow-md sm:rounded-lg overflow-scroll">
-      <table class="w-full overflow-x-auto text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <div className=" p-12 shadow-md sm:rounded-lg overflow-y-scroll md:overflow-y-hidden">
+      <table className="w-full overflow-x-auto text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" class="px-4 py-2">
+            <th scope="col" className="px- md:px-6 ml-4 py-2">
               Coin
             </th>
-            <th scope="col" class="px-4 py-2">
+            <th scope="col" className="px-4 md:px-6 ml-4 py-2">
               Color
             </th>
-            <th scope="col" class="px-4 py-2">
+            <th scope="col" className="px-4 md:px-6 ml-4 py-2">
               Price
             </th>
-            <th scope="col" class="px-4 py-2">
+            <th scope="col" className="px-4 md:px-6 ml-4 py-2">
               24%h
             </th>
-            <th scope="col" class="px-4 py-2">
+            <th scope="col" className="px-4 md:px-6 ml-4 py-2">
               Volume
             </th>
-            <th scope="col" class="px-4 py-2">
+            <th scope="col" className="px-4 md:px-6 ml-4 py-2">
               MarketCap
             </th>
           </tr>
